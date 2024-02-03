688ad9a6659ee431e071a03fe7dbf54b7f461196
Your Name <you@example.com>
2024-02-04 01:16:06 +0330

    add some field for Customer

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,4 +1,24 @@
 package model;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
 public class Customer {
+
+  String name;
+  String lastName;
+  String username;
+  String email;
+  String password;
+  String nationalCode;
+
+
+
+
 }
