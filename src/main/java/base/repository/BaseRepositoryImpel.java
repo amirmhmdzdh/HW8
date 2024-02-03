8fa4f467deeacb7869ac7f209c3d70e1d2ad51d8
Your Name <you@example.com>
2024-02-04 00:05:33 +0330

    add save query

--- src/main/java/base/repository/BaseRepositoryImpel.java
+++ src/main/java/base/repository/BaseRepositoryImpel.java
@@ -4,10 +4,13 @@ import base.model.BaseEntity;
 
 import java.io.Serializable;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public abstract class BaseRepositoryImpel<ID extends Serializable, TYPE extends BaseEntity<ID>> implements BaseRepository<ID, TYPE> {
 
- private final Connection connection;
+    private final Connection connection;
 
 
     protected BaseRepositoryImpel(Connection connection) {
@@ -15,16 +18,46 @@ public abstract class BaseRepositoryImpel<ID extends Serializable, TYPE extends 
     }
 
 
+    @Override
+    public void save(TYPE entity) throws SQLException {
+
+        String sql = " INSER INTO " + getTableName() + " " + getColumnsName() + " VALUES " + getCountOfQestionMarkParams();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            fillParamForStatement(preparedStatement, entity, false);
+            preparedStatement.executeUpdate();
+
+        }
+
+    }
+
+    @Override
+    public TYPE findById(ID id) {
+        return null;
+    }
+
+    @Override
+    public void update(TYPE entity) {
+
+    }
+
+    @Override
+    public void delete(ID id) {
+
+    }
 
 
+    public abstract String getTableName();
 
+    public abstract String getColumnsName();
 
+    public abstract String getCountOfQestionMarkParams();
 
+    public abstract void fillParamForStatement(PreparedStatement preparedStatement, TYPE entity, boolean isCountOnly) throws SQLException;
 
+    public abstract TYPE mapResultSetToEntity(ResultSet resultSet) throws SQLException;
 
-
-
-
+    public abstract String getUpdateQueryParams();
 
 
 }
