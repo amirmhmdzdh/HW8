c4b39ef793c931ad9c1bc895846556e888c00931
Your Name <you@example.com>
2024-02-04 00:12:52 +0330

    add 4 methd in interface BaseService

--- src/main/java/base/service/BaseService.java
+++ src/main/java/base/service/BaseService.java
@@ -1,4 +1,19 @@
 package base.service;
 
-public interface BaseService {
+import base.model.BaseEntity;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+
+public interface BaseService<ID extends Serializable, TYPE extends BaseEntity<ID>> {
+
+    void save(TYPE entity) throws SQLException;
+
+    TYPE findById(ID id) throws SQLException;
+
+    void update(TYPE entity) throws SQLException;
+
+    void delete(ID id) throws SQLException;
+
+
 }
