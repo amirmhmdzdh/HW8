fb164be10d00e2bbcd95b06e34c1a47364664c75
Your Name <you@example.com>
2024-02-03 23:51:12 +0330

    add field id in BaseEntity

--- src/main/java/base/model/BaseEntity.java
+++ src/main/java/base/model/BaseEntity.java
@@ -1,4 +1,18 @@
 package base.model;
 
-public class BaseEntity {
+import lombok.*;
+
+import java.io.Serializable;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+
+
+public class BaseEntity<ID extends Serializable> {
+
+    private ID id;
+
 }
