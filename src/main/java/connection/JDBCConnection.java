723f72375cfee645effc0dbe27f423070bd38840
Your Name <you@example.com>
2024-02-04 01:08:58 +0330

    add connection and url

--- src/main/java/connection/JDBCConnection.java
+++ src/main/java/connection/JDBCConnection.java
@@ -1,4 +1,27 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCConnection {
+
+
+    private static Connection connection = null;
+
+    private JDBCConnection() {
+    }
+
+    public static Connection getConnection() {
+
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "Amir");
+            } catch (SQLException e) {
+                e.getMessage();
+            }
+        }
+        return connection;
+    }
+
 }
