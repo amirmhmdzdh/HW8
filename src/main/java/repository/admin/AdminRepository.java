9daa78f3d761cd50fcdf10110ced8e3703eb62cf
Your Name <you@example.com>
2024-02-05 01:27:14 +0330

    Create AdminRepository and add field

---
+++ src/main/java/repository/admin/AdminRepository.java
@@ -0,0 +1,14 @@
+package repository.admin;
+
+import base.repository.BaseRepository;
+import model.Admin;
+
+public interface AdminRepository extends BaseRepository<Integer , Admin > {
+
+
+
+
+
+
+
+}
