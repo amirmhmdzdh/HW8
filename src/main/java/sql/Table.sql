f76821fd25fa37611e20bb9840241d9a54d01f4b
Your Name <you@example.com>
2024-02-05 01:10:27 +0330

    add table admin

--- src/main/java/sql/Table.sql
+++ src/main/java/sql/Table.sql
@@ -1,11 +1,19 @@
 CREATE TABLE IF NOT EXISTS customer
 (
 
-    id           SERIAL PRIMARY KEY,
+    id            SERIAL PRIMARY KEY,
     first_name    VARCHAR,
     last_name     VARCHAR,
-    user_name     VARCHAR,
-    email        VARCHAR,
-    password     VARCHAR,
-    national_code VARCHAR
+    user_name     VARCHAR UNIQUE,
+    email         VARCHAR UNIQUE,
+    password      VARCHAR,
+    national_code VARCHAR UNIQUE
+);
+CREATE TABLE IF NOT EXISTS admin
+(
+    id        SERIAL PRIMARY KEY,
+    user_name VARCHAR unique,
+    password  VARCHAR unique
+
+
 );
