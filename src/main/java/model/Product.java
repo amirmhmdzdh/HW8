91a5b3f85129da96e41aadbb4e7e47e5ff4490c8
Your Name <you@example.com>
2024-02-06 12:29:17 +0330

    add method constructor , TOstring

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -9,7 +9,7 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-
+@ToString(callSuper = true)
 
 public class Product extends BaseEntity<Integer> {
 
@@ -21,4 +21,14 @@ public class Product extends BaseEntity<Integer> {
     int price;
     int categoryId;
 
+    public Product(int productId, String name, String description, String gender, String color, int size, int price, int categoryId) {
+        super(productId);
+        this.name = name;
+        this.description = description;
+        this.gender = gender;
+        this.color = color;
+        this.siza = size;
+        this.price = price;
+        this.categoryId = categoryId;
+    }
 }
