4f069e1693df71ca4ed178013881d6984239158c
Your Name <you@example.com>
2024-02-06 12:09:22 +0330

    add Constructor for Category

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -9,11 +9,19 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-
+@ToString(callSuper = true)
 public class Category extends BaseEntity<Integer> {
 
     String name;
     String description;
 
 
+    public Category(int categoryId, String categoryName, String description) {
+        super(categoryId);
+        this.name = categoryName;
+        this.description = description;
+    }
 }
+
+
+
