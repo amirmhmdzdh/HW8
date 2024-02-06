0f90a7690b3a6d6ba680ebc5d7e5d55c1afaeab6
Your Name <you@example.com>
2024-02-06 12:10:20 +0330

    add method findAllCategory

--- src/main/java/repository/category/CategoryRepository.java
+++ src/main/java/repository/category/CategoryRepository.java
@@ -3,5 +3,10 @@ package repository.category;
 import base.repository.BaseRepository;
 import model.Category;
 
+import java.sql.SQLException;
+
 public interface CategoryRepository extends BaseRepository<Integer , Category> {
+
+ Category[] findAllCategory() throws SQLException;
+
 }
