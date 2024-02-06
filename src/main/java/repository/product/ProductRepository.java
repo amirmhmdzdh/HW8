b7530170485a40b9b387aa8baaf849f04ba6d45c
Your Name <you@example.com>
2024-02-06 12:29:52 +0330

    add method findAllProduct

--- src/main/java/repository/product/ProductRepository.java
+++ src/main/java/repository/product/ProductRepository.java
@@ -3,5 +3,10 @@ package repository.product;
 import base.repository.BaseRepository;
 import model.Product;
 
+import java.sql.SQLException;
+
 public interface ProductRepository extends BaseRepository<Integer, Product> {
+
+ Product[] findAllProduct() throws SQLException;
+
 }
