3fa58fd855ec76a2f90c0f411aae053ad38fd379
Your Name <you@example.com>
2024-02-08 00:38:09 +0330

    add findByUser

--- src/main/java/base/service/BaseServiceImpel.java
+++ src/main/java/base/service/BaseServiceImpel.java
@@ -2,7 +2,6 @@ package base.service;
 
 import base.model.BaseEntity;
 import base.repository.BaseRepository;
-
 import java.io.Serializable;
 import java.sql.SQLException;
 
@@ -14,26 +13,19 @@ public class BaseServiceImpel<ID extends Serializable, TYPE extends BaseEntity<I
         this.repository = repository;
     }
 
-
     @Override
     public void save(TYPE entity) throws SQLException {
         repository.save(entity);
     }
 
     @Override
-    public TYPE findByUser(ID id) throws SQLException {
-        return null;
+    public TYPE findByUser(String entity) throws SQLException {
+        return repository.findUser(entity);
     }
 
     @Override
     public TYPE findById(ID id) throws SQLException {
         return repository.findById(id);
-    }
-
-
-    @Override
-    public TYPE findByUser(String username) throws SQLException {
-        return repository.findUser(username);
     }
 
     @Override
